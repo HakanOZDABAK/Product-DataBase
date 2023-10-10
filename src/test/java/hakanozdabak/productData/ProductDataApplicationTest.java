764c02e75c749dedc2de6e4895746db73ef5bca7
HakanOZDABAK <hakanozdabak2000@hotmail.com>
2023-10-10 12:26:23 +0300

    Added "ProductManagerTest" Test ProductM Manager Test unit added, ProductDataApplicationTest

---
+++ src/test/java/hakanozdabak/productData/ProductDataApplicationTest.java
@@ -0,0 +1,13 @@
+package hakanozdabak.productData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ProductDataApplicationTest {
+
+    @Test
+    void contextLoad(){
+
+    }
+}
